a11c51b6260f184f09a51b8229351f8527702730
Liam-McHara <guillem@lajara.re>
2024-11-07 17:28:14 +0100

    FIXED minimap colors

--- inc/minimap.h
+++ inc/minimap.h
@@ -1,7 +1,7 @@
 #include "cub3d.h"
 
 #define MAP_PLAYER_COLOR 0x00FF00FF
-#define MAP_FLOOR_COLOR 0x000000
-#define MAP_WALL_COLOR 0xFFFFFF
+#define MAP_FLOOR_COLOR 0x000000FF
+#define MAP_WALL_COLOR 0xFFFFFFFF
 
 void    draw_minimap(t_cub *c);
