37971ebca7f2f110c750bac4b0a558fd757b7008
Liam McHara <guillemlr@proton.me>
2024-11-11 17:48:41 +0100

    Adjusted player speed and rotation

--- inc/movements.h
+++ inc/movements.h
@@ -2,7 +2,7 @@
 #include "assets.h"
 
 # define PLAYER_SPEED 4.0
-# define ROTATION_SPEED 3.0
+# define ROTATION_SPEED 2.0
 # define MOUSE_ROT_SPEED 0.2
 # define RADIUS 0.25
 
@@ -17,6 +17,4 @@ typedef struct s_matrix
 void	upd_player_pos(t_cub *c, int key);
 void 	mouse_rotation(t_cub *c);
 void	rot_player(t_cub *c, double angle);
-
-// private
 void	player_move_pos(t_cub *c, int key, t_vec2_d *new_pos);
