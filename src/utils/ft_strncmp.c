41428acec1523e1eef18c01c3de15206f2e556a8
Liam-McHara <guillem@lajara.re>
2024-11-12 16:14:07 +0100

    FIXED ft_strncmp

--- src/utils/ft_strncmp.c
+++ src/utils/ft_strncmp.c
@@ -4,11 +4,14 @@
 // or a positive value if 's1' is equal, less or greater than 's2'.
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n)
+	size_t	i;
+
+	i = 0;
+	while (i < n)
 	{
-		if (!*s1 || !*s2 || *s1 != *s2)
-			return ((unsigned char) *s1 - (unsigned char) *s2);
-		--n;
+		if (!s1[i] || !s2[i] || s1[i] != s2[i])
+			return ((unsigned char) s1[i] - (unsigned char) s2[i]);
+		++i;
 	}
 	return (0);
 }
