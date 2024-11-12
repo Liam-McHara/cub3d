b0c4594e64b2b10e9df4bd11c62b43ab68600b35
Liam-McHara <guillem@lajara.re>
2024-11-12 18:37:55 +0100

    UPDATED minimap: player centered

--- inc/minimap.h
+++ inc/minimap.h
@@ -1,7 +1,8 @@
 #include "cub3d.h"
 
 #define MAP_PLAYER_COLOR 0x00FF00FF
-#define MAP_FLOOR_COLOR 0x000000FF
-#define MAP_WALL_COLOR 0xFFFFFFFF
+#define MAP_F_COLOR 0x000000FF
+#define MAP_W_COLOR 0xFFFFFFFF
+#define MAP_PXL_SIZE 6
 
 void    minimap(t_cub *c);
--- src/minimap.c
+++ src/minimap.c
@@ -1,26 +1,26 @@
 #include "cub3d.h"
-#include "minimap.h"		// MAP_*_COLOR
+#include "minimap.h"		// MAP_*
 
 void	mini_draw(t_cub *c, int x, int y, int color)
 {
 	int	x_max;
 	int	y_max;
 
-	x_max = x + 5;
-	y_max = y + 5;
+	x_max = x + MAP_PXL_SIZE;
+	y_max = y + MAP_PXL_SIZE;
 	while (y < y_max && (uint32_t)y < c->img->width)
 	{
 		while (x < x_max && (uint32_t)x < c->img->height)
 			mlx_put_pixel(c->img, x++, y, color);
-		x -= 5;
+		x -= MAP_PXL_SIZE;
 		y++;
 	}
 }
 
 void	draw_minimap(t_cub *c)
 {
-	int		x;
-	int		y;
+	int	x;
+	int	y;
 
 	y = 0;
 	while (c->map[y])
@@ -29,9 +29,9 @@ void	draw_minimap(t_cub *c)
 		while (c->map[y][x])
 		{
 			if (c->map[y][x] == WALL)
-				mini_draw(c, x * 5, y * 5, MAP_WALL_COLOR);
+				mini_draw(c, x * MAP_PXL_SIZE, y * MAP_PXL_SIZE, MAP_W_COLOR);
 			else if (c->map[y][x] == FLOOR)
-				mini_draw(c, x * 5, y * 5, MAP_FLOOR_COLOR);
+				mini_draw(c, x * MAP_PXL_SIZE, y * MAP_PXL_SIZE, MAP_F_COLOR);
 			x++;
 		}
 		y++;
@@ -40,6 +40,11 @@ void	draw_minimap(t_cub *c)
 
 void	minimap(t_cub *c)
 {
+	int	x;
+	int	y;
+
+	x = c->player.pos.x * MAP_PXL_SIZE - (MAP_PXL_SIZE / 2);
+	y = c->player.pos.y * MAP_PXL_SIZE - (MAP_PXL_SIZE / 2);
 	draw_minimap(c);
-	mini_draw(c, c->player.pos.x * 5, c->player.pos.y * 5, MAP_PLAYER_COLOR);
+	mini_draw(c, x, y, MAP_PLAYER_COLOR);
 }
